ab2d8e874d11befed6d831f9cf3a0d54b8514227
BradleyParkerDev <bradleyscottparker@outlook.com>
2026-04-21 16:35:40 -0500

    update controller index file

--- app/server/src/controllers/index.ts
+++ app/server/src/controllers/index.ts
@@ -17,15 +17,15 @@ import getCurrentPageState from './ui/getCurrentPageState.js';
 import updateUserTheme from './ui/updateUserTheme.js';
 
 // news controller
-import getTopHeadlinesArticles from './news/topHeadlines/topHeadlines.js';
-import getBusinessArticles from './news/business/business.js';
-import getEntertainmentArticles from './news/entertainment/entertainment.js';
-import getGeneralArticles from './news/general/general.js';
-import getHealthArticles from './news/health/health.js';
-import getSavedArticles from './news/savedArticles/savedArticles.js';
-import getScienceArticles from './news/science/science.js';
-import getSportsArticles from './news/sports/sports.js';
-import getTechnologyArticles from './news/technology/technology.js';
+import getTopHeadlinesArticles from './news/topHeadlines.js';
+import getBusinessArticles from './news/business.js';
+import getEntertainmentArticles from './news/entertainment.js';
+import getGeneralArticles from './news/general.js';
+import getHealthArticles from './news/health.js';
+import getSavedArticles from './news/savedArticles.js';
+import getScienceArticles from './news/science.js';
+import getSportsArticles from './news/sports.js';
+import getTechnologyArticles from './news/technology.js';
 
 // web controller import
 import web from './web/index.js';
